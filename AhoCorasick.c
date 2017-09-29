295423007ee6dcae3c5e9290b89606993d61dbea
Ptilulu83 <lucas.moragues@wanadoo.fr>
2017-09-29 17:39:56 +0200

    new tableau_commande
    utilisation de strncmp() pour la comparaison des strings
    + le ; oublier
    + remplacement des 4x" " par une tabulation :p

--- AhoCorasick.c
+++ AhoCorasick.c
@@ -10,126 +10,133 @@
 #include <string.h>
 
 char * ajouteprefix(int size,char * mot){
-    char *prefix=malloc(sizeof(char)*size);
-    memcpy(prefix, mot, size);
-    return prefix;
+	char *prefix=malloc(sizeof(char)*size);
+	memcpy(prefix, mot, size);
+	return prefix;
 }
 
 char ** genere_prefix(char * mots[],int start,int end,int * nb_etats){			//argv,2,argc, un pointeur sur un entier
-    int nbprefixmax=0,sizeprefixmax=0;
-    for (int nummots=start; nummots < end; ++nummots){
-        int taillemot=strlen(mots[nummots]);
-        nbprefixmax+=taillemot;
-        if (sizeprefixmax < taillemot){
-            sizeprefixmax = taillemot;
-        }
-    }
-    printf("sizeprefixmax = %d , nbprefixmax = %d \n",sizeprefixmax,nbprefixmax);		// affichage première partie 
-    char ** prefix = malloc(sizeof(char *)*nbprefixmax);
-    if(prefix==NULL){
-        perror("malloc");
-        exit(1);
-    }
-    int isfirst;
-    int etats=0;
-    for (int sizeprefix = 1; sizeprefix <= sizeprefixmax; ++sizeprefix){		// genere les prefix taille par taille
-        isfirst=1;
-        for (int nummots = start; nummots < end; ++nummots){	//parcour tout les mots
-            if(sizeprefix <= strlen(mots[nummots])){
-                if(isfirst){													//premier mot de cette taile
-                    isfirst=0;
-                    prefix[etats]=ajouteprefix(sizeprefix,mots[nummots]);
-                    etats++;
-                }
-                else {
-                	int tmp=strncmp(mots[nummots],prefix[etats-1],sizeprefix);	//comparaison des deux string jusqu'a sizeprefix
-                	if (tmp > 0){												
-                		prefix[etats]=ajouteprefix(sizeprefix,mots[nummots]);
-                		etats++;
-                	}
-                	else if(tmp < 0){											//tmp est censer etre positif vu qu'on a trié avant
-                		perror("tris ascii")
-                	}
+	int nbprefixmax=0,sizeprefixmax=0;
+	for (int nummots=start; nummots < end; ++nummots){
+		int taillemot=strlen(mots[nummots]);
+		nbprefixmax+=taillemot;
+		if (sizeprefixmax < taillemot){
+			sizeprefixmax = taillemot;
+		}
+	}
+	printf("sizeprefixmax = %d , nbprefixmax = %d \n",sizeprefixmax,nbprefixmax);		// affichage première partie 
+	char ** prefix = malloc(sizeof(char *)*nbprefixmax);
+	if(prefix==NULL){
+		perror("malloc");
+		exit(1);
+	}
+	int isfirst;
+	int etats=0;
+	for (int sizeprefix = 1; sizeprefix <= sizeprefixmax; ++sizeprefix){		// genere les prefix taille par taille
+		isfirst=1;
+		for (int nummots = start; nummots < end; ++nummots){	//parcour tout les mots
+			if(sizeprefix <= strlen(mots[nummots])){
+				if(isfirst){													//premier mot de cette taile
+					isfirst=0;
+					prefix[etats]=ajouteprefix(sizeprefix,mots[nummots]);
+					etats++;
+				}
+				else {
+					int tmp=strncmp(mots[nummots],prefix[etats-1],sizeprefix);	//comparaison des deux string jusqu'a sizeprefix
+					if (tmp > 0){												
+						prefix[etats]=ajouteprefix(sizeprefix,mots[nummots]);
+						etats++;
+					}
+					else if(tmp < 0){											//tmp est censer etre positif vu qu'on a trié avant
+						perror("tris ascii");
+					}
 
-                	/*
-                    for (int j = 0; j < sizeprefix; ++j){					//compare toute les lettres au mot précédent  vielle version a la main
-                        if ( prefix[etats-1][j]!=mots[nummots][j]){
-                            prefix[etats]=ajouteprefix(sizeprefix,mots[nummots]);
-                            etats++;
-                            break;
-                        }
-                    }
-                    */
+					/*
+					for (int j = 0; j < sizeprefix; ++j){						//compare toute les lettres au mot précédent  			vielle version lettre par lettre a la main
+						if ( prefix[etats-1][j]!=mots[nummots][j]){
+							prefix[etats]=ajouteprefix(sizeprefix,mots[nummots]);
+							etats++;
+							break;
+						}
+					}
+					*/
 
-                }
-            }
-        }
-    }
-    *nb_etats=etats;
-    return prefix;
+				}
+			}
+		}
+	}
+	*nb_etats=etats;
+	return prefix;
 }
 
 int ** tableau_commande(char ** prefix,int nb_etats){
-    int ** commande=malloc(sizeof(int *)*nb_etats+1);
-    if(commande==NULL){
-        perror("malloc");
-        exit(1);
-    }
-    for (int i = 0; i < nb_etats+1; ++i){
-        commande[i]=calloc(26,sizeof(int));
-        if(commande[i]==NULL){
-            perror("calloc");
-            exit(1);
-        }
-    }
-    int sizeprefix=1,firstprefix=-1,nextfirstprefix=0;
-    for (int numprefix = 0; numprefix < nb_etats; ++numprefix){
-        if(strlen(prefix[numprefix])==sizeprefix){
-            if(sizeprefix==1){
-                commande[0][prefix[numprefix][0]-'a']=numprefix+1;			// met le numérot de l'etat correspondant au prefix dans la case de la lette/0 (prefix[numprefix][sizeprefix-1]-'a')
-            }
-            else{
-                int preprefix=-1;
-                for (int i = firstprefix; preprefix==-1 && i < nextfirstprefix; ++i){
-                    for (int lettre = 0; lettre < sizeprefix-1; ++lettre){				// on compare les prefix précédent au préfix actuel
-                        if(prefix[i][lettre]!=prefix[numprefix][lettre]){
-                            break;
-                        }
-                        else if(lettre==sizeprefix-2){
-                            preprefix=i;												// on trouve le préfix du préfix
-                        }
-                    }
-                }
-                if (preprefix!=-1){
-                    commande[preprefix+1][prefix[numprefix][sizeprefix-1]-'a']=numprefix+1;	// met le numérot de l'etat correspondant au prefix dans la case de la lette/preprefix (prefix[numprefix][sizeprefix-1]-'a')
-                }
-                else{
-                    printf("%s %s %s \n",prefix[numprefix],prefix[firstprefix],prefix[nextfirstprefix]);
-                    perror("commande");
-                }
-            }
-        }
-        else{													//on change et on recommence car prefix de taille supérieur
-            sizeprefix++;
-            firstprefix=nextfirstprefix;
-            nextfirstprefix=numprefix;
-            numprefix--;												// est-ce moche ?
-        }
-    }
-    return commande;
+	int ** commande=malloc(sizeof(int *)*nb_etats+1);
+	if(commande==NULL){
+		perror("malloc");
+		exit(1);
+	}
+	for (int i = 0; i < nb_etats+1; ++i){
+		commande[i]=calloc(26,sizeof(int));
+		if(commande[i]==NULL){
+			perror("calloc");
+			exit(1);
+		}
+	}
+	int sizeprefix=1,firstprefix=-1,nextfirstprefix=0;
+	for (int numprefix = 0; numprefix < nb_etats; ++numprefix){
+		if(strlen(prefix[numprefix])==sizeprefix){
+			if(sizeprefix==1){
+				commande[0][prefix[numprefix][0]-'a']=numprefix+1;			// met le numérot de l'etat correspondant au prefix dans la case de la lette/0 (prefix[numprefix][sizeprefix-1]-'a')
+			}
+			else{
+				int preprefix=-1;
+				for (int i = firstprefix; preprefix==-1 && i < nextfirstprefix; ++i){
+					int tmp=strncmp(prefix[numprefix],prefix[i],sizeprefix-1);	//comparaison des deux string jusqu'a sizeprefix-1
+					if(tmp==0){
+						preprefix=i;											// on trouve le préfix du préfix
+					}
+
+					/*
+					for (int lettre = 0; lettre < sizeprefix-1; ++lettre){				// on compare les prefix précédent au préfix actuel			vielle version lettre par lettre
+						if(prefix[i][lettre]!=prefix[numprefix][lettre]){
+							break;
+						}
+						else if(lettre==sizeprefix-2){
+							preprefix=i;												// on trouve le préfix du préfix
+						}
+					}
+					*/
+				}
+				if (preprefix!=-1){
+					commande[preprefix+1][prefix[numprefix][sizeprefix-1]-'a']=numprefix+1;	// met le numérot de l'etat correspondant au prefix dans la case de la lette/preprefix (prefix[numprefix][sizeprefix-1]-'a')
+				}
+				else{
+					printf("%s %s %s \n",prefix[numprefix],prefix[firstprefix],prefix[nextfirstprefix]);			//perror si on a pas trouver le prefix du prefix
+					perror("commande");
+				}
+			}
+		}
+		else{													//on change et on recommence car prefix de taille supérieur
+			sizeprefix++;
+			firstprefix=nextfirstprefix;
+			nextfirstprefix=numprefix;
+			numprefix--;												// est-ce moche ?
+		}
+	}
+	return commande;
 }
 
 void show_tableau_commande(int ** commande, int nb_etats){
-    for (int j = 0; j < 26; ++j){
-        printf("\t%c",j+'a' );
-    }
-    for (int i = 0; i < nb_etats+1; ++i){
-        printf("%d :\t", i);
-        for (int j = 0; j < 26; ++j){
-            printf("%d\t", commande[i][j]);
-        }
-        printf("\n");
-    }
+	for (int j = 0; j < 26; ++j){
+		printf("\t%c",j+'a' );
+	}
+	for (int i = 0; i < nb_etats+1; ++i){
+		printf("%d :\t", i);
+		for (int j = 0; j < 26; ++j){
+			printf("%d\t", commande[i][j]);
+		}
+		printf("\n");
+	}
 }
 
 /*int ** tableau_erreur(){
